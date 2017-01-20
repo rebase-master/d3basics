be2c4c0cfc38f256505b9a5a6f3e03543005a95f
steady-daddy <thisismansoorkhan@gmail.com>
2017-01-20 21:32:16 +0530

    3 dynamic circles

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -2,20 +2,21 @@ var dataArray = [5,11,18];
 
 var svg = d3.select('body').append('svg').attr('height','100%').attr('width','100%');
 
+var newX = 300;
+
 svg.selectAll('rect')
     .data(dataArray)
     .enter()
-    .append('rect')
-        .attr('height',function (d, i) {
-            return d*15;
+    .append('circle')
+        .attr('cx',function (d, i) {
+            newX+= (d*6) + (i*20);
+            return newX;
         })
-        .attr('width',50)
-        .attr('fill', 'pink')
-        .attr('x',function (d, i) {
-            return 60*i;
+        .attr('cy',function (d, i) {
+            return 100;
         })
-        .attr('y',function (d, i) {
-            return 300 - d*15;
+        .attr('r',function (d) {
+            return d*3;
         })
 
 ;
