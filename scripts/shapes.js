a5d7a43e39ab7b07751e2103dff87f5ec98e768b
steady-daddy <thisismansoorkhan@gmail.com>
2017-01-20 21:53:50 +0530

    Creating and styling lines

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -5,21 +5,22 @@ var svg = d3.select('body').append('svg').attr('height','100%').attr('width','10
 var newX = 300;
 
 
-svg.selectAll('ellipse')
+svg.selectAll('line')
     .data(dataArray)
-    .enter().append('ellipse')
-            .attr('class','second')
-            .attr('cx',function (d, i) {
-                newX+= (d*6) + (i*20);
+    .enter().append('line')
+            .attr('x1',function (d, i) {
                 return newX;
             })
-            .attr('cy',function (d, i) {
-                return 100;
+            .attr('y1',function (d, i) {
+                return 80+(i*20);
             })
-            .attr('rx',function (d) {
-                return d*3;
+            .attr('x2',function (d) {
+                return newX + d*15;
             })
-            .attr('ry',function (d) {
-                return 30;
+            .attr('y2',function (d,i) {
+                return 80+(i*20);
             })
+            .style('stroke', 'green')
+            // .attr('stroke', 'blue')
+            .attr('stroke-width', 2)
 ;
