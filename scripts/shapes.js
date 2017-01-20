bb78c98267b17f2b2e4b3a176414aa545f399964
steady-daddy <thisismansoorkhan@gmail.com>
2017-01-20 21:42:08 +0530

    3 dynamic ellipses

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -4,26 +4,10 @@ var svg = d3.select('body').append('svg').attr('height','100%').attr('width','10
 
 var newX = 300;
 
-svg.selectAll('circle.first')
-    .data(dataArray)
-    .enter().append('circle')
-            .attr('class','first')
-            .attr('cx',function (d, i) {
-                newX+= (d*6) + (i*20);
-                return newX;
-            })
-            .attr('cy',function (d, i) {
-                return 100;
-            })
-            .attr('r',function (d) {
-                return d*3;
-            })
-;
- newX = 600;
 
-svg.selectAll('circle.second')
+svg.selectAll('ellipse')
     .data(dataArray)
-    .enter().append('circle')
+    .enter().append('ellipse')
             .attr('class','second')
             .attr('cx',function (d, i) {
                 newX+= (d*6) + (i*20);
@@ -32,7 +16,10 @@ svg.selectAll('circle.second')
             .attr('cy',function (d, i) {
                 return 100;
             })
-            .attr('r',function (d) {
+            .attr('rx',function (d) {
                 return d*3;
+            })
+            .attr('ry',function (d) {
+                return 30;
             })
 ;
