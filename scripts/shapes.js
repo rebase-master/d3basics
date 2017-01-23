30d3bb26728ff81d48a83578a820572bd6bbfb54
steady-daddy <thisismansoorkhan@gmail.com>
2017-01-23 11:29:21 +0530

    Area chart Dynamic data

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -1,20 +1,20 @@
-var dataArray = [{x:5,y:5},{x:10,y:15},{x:20,y:7},{x:30,y:18},{x:40,y:10}];
+var dataArray = [25,26,28,32,37,45,55,70,90,120,135,150,160, 168,172,177,180];
+var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016'];
+
+var height = 200;
+var width = 500;
+
+var area = d3.area()
+                .x(function (d, i) {
+                    return i*20;
+                })
+                .y0(height) //height of the chart
+                .y1(function (d) {
+                    return height - d;
+                }) // draw upper boundary of the chart
+;
 
 var svg = d3.select('body').append('svg').attr('height', '100%').attr('width','100%');
 
-var line = d3.line()
-             .x(function (d, i) {
-                 return d.x * 6;
-             })
-             .y(function (d, i) {
-                 return d.y * 6;
-             })
-    // .curve(d3.curveStep)
-    .curve(d3.curveCardinal)
-;
-
-svg.append('path')
-    .attr('fill','none')
-    .attr('stroke', "blue")
-    .attr('d', line(dataArray))
+svg.append('path').attr('d', area(dataArray))
 ;
